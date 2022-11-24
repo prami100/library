1326436e329203c791fd65ede9db640493297675
prami100 <118107563+prami100@users.noreply.github.com>
2022-11-24 22:39:46 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,14 @@
+def sum(a,b)
+{
+    echo "sum of ${a} and ${b} is ${a+b}"
+}
+
+def sub(a.b)
+{
+    echo "difference of ${a} and ${b} is ${a-b}"
+}
+
+def mul(a,b)
+{
+    echo "product of ${a} and ${b} is ${a*b}"
+}
